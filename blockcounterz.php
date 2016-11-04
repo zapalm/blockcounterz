3d7d0714175665d51cd8b4562ae0387d0deaea04
zapalm <zapalm@ya.ru>
2016-11-05 07:52:52 +1000

    // release of 1.1.0 : save the content to DB; refactoring directory structure

--- blockcounterz.php
+++ blockcounterz.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Block Counterz: module for PrestaShop 1.2-1.6
+ * Block counters: module for PrestaShop 1.2-1.6
  *
- * @author zapalm <zapalm@ya.ru>
- * @copyright (c) 2010-2015, zapalm
- * @link http://prestashop.modulez.ru/en/ The module's homepage
- * @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+ * @author    zapalm <zapalm@ya.ru>
+ * @copyright (c) 2010-2016, zapalm
+ * @link      http://prestashop.modulez.ru/en/frontend-features/43-block-counters.html The module's homepage
+ * @license   http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
  */
 
 if (!defined('_PS_VERSION_'))
@@ -13,46 +13,42 @@ if (!defined('_PS_VERSION_'))
 
 class BlockCounterz extends Module
 {
-	private $counters_filepath;
+	const CONF_CONTENT = 'BLOCKCOUNTERZ_CONTENT';
 
-	public function __construct()
-	{
+	public function __construct() {
 		$this->name = 'blockcounterz';
 		$this->tab = 'Blocks';
-		$this->version = '1.0.0';
+		$this->version = '1.1.0';
 		$this->author = 'zapalm';
 
 		parent::__construct();
 
 		$this->displayName = $this->l('Stat counters block');
 		$this->description = $this->l('Adds a block to display stat counters.');
-
-		$this->counters_filepath = dirname(__FILE__).'/counters.html';
 	}
 
-	private function getCountersCode()
-	{
-		if (file_exists($this->counters_filepath))
-			return file_get_contents($this->counters_filepath);
+	public function install() {
+		$content = base64_encode('<!--noindex-->' . PHP_EOL . '<!--/noindex-->');
+		Configuration::updateValue(self::CONF_CONTENT, $content);
+
+		return parent::install()
+			&& $this->registerHook('header')
+			&& $this->registerHook('footer')
+		;
 	}
 
-	public function install()
-	{
-		if (!file_exists($this->counters_filepath))
-			file_put_contents($this->counters_filepath, '<!--noindex-->'."\r\n".'<!--/noindex-->');
+	public function uninstall() {
+		Configuration::deleteByName(self::CONF_CONTENT);
 
-		return parent::install() && $this->registerHook('rightColumn');
+		return parent::uninstall();
 	}
 
-	public function getContent()
-	{
-		global $cookie;
-
+	public function getContent() {
 		$output = '';
 
-		if (Tools::isSubmit('submit_save'))
-		{
-			if (file_put_contents($this->counters_filepath, Tools::getValue('counters_text')))
+		if (Tools::isSubmit('submit_save')) {
+			$content = base64_encode(trim(Tools::getValue('counters_text')));
+			if (Configuration::updateValue(self::CONF_CONTENT, $content))
 				$output .= $this->displayConfirmation($this->l('Settings updated'));
 			else
 				$output .= $this->displayError($this->l('Some setting not updated'));
@@ -63,7 +59,7 @@ class BlockCounterz extends Module
 				<fieldset>
 					<legend><img src="'._PS_ADMIN_IMG_.'cog.gif" alt="" title="" />'.$this->l('Settings').'</legend>
 					<b>'.$this->l('Stat counters code').':</b><br />
-					<textarea style="width:100%; height:300px;" name="counters_text">'.$this->getCountersCode().'</textarea><br />
+					<textarea style="width:100%; height:300px;" name="counters_text">'.base64_decode(Configuration::get(self::CONF_CONTENT)).'</textarea><br />
 					<center><input type="submit" name="submit_save" value="'.$this->l('Save').'" class="button" /></center>
 				</fieldset>
 			</form>
@@ -73,22 +69,31 @@ class BlockCounterz extends Module
 		return $output;
 	}
 
-	public function hookRightColumn($params)
-	{
+	public function hookHeader() {
+		return '<link href="' . $this->_path . 'views/css/main.css" rel="stylesheet">';
+	}
+
+	public function hookRightColumn($params) {
 		global $smarty;
 
-		$smarty->assign('stat_counters', $this->getCountersCode());
+		$smarty->assign(array(
+			'stat_counters' => base64_decode(Configuration::get(self::CONF_CONTENT)),
+		));
 
-		return $this->display(__FILE__, 'blockcounterz.tpl');
+		return $this->display(__FILE__, 'views/templates/hook/column-right.tpl');
 	}
 
-	public function hookLeftColumn($params)
-	{
+	public function hookLeftColumn($params) {
 		return $this->hookRightColumn($params);
 	}
 
-	public function hookFooter($params)
-	{
-		return $this->hookRightColumn($params);
+	public function hookFooter($params) {
+		global $smarty;
+
+		$smarty->assign(array(
+			'stat_counters' => base64_decode(Configuration::get(self::CONF_CONTENT)),
+		));
+
+		return $this->display(__FILE__, 'views/templates/hook/footer.tpl');
 	}
 }
---
+++ views/templates/hook/footer.tpl
@@ -0,0 +1,14 @@
+{**
+* Block counters: module for PrestaShop 1.2-1.6
+*
+* @author    zapalm <zapalm@ya.ru>
+* @copyright (c) 2010-2015, zapalm
+* @link      http://prestashop.modulez.ru/en/frontend-features/43-block-counters.html The module's homepage
+* @license   http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+*}
+
+<!-- MODULE: blockcounterz -->
+<div class="stat-counters-block-footer col-xs-12">
+    {$stat_counters}
+</div>
+<!-- /MODULE: blockcounterz -->
