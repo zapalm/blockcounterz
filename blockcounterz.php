2e77bf7b199f2ee642644a59e6dfa47785dd89d0
zapalm <zapalm@ya.ru>
2021-08-19 14:25:12 +1000

    Release of 1.4.0

--- blockcounterz.php
+++ blockcounterz.php
@@ -34,7 +34,7 @@ class BlockCounterz extends Module
     {
         $this->name    = 'blockcounterz';
         $this->tab     = 'Blocks';
-        $this->version = '1.3.0';
+        $this->version = '1.4.0';
         $this->author  = 'zapalm';
 
         parent::__construct();
